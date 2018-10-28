a6ee73d021e4bf24932f8abf8db55d90f6c8bb17
dnovik <dnovik01@gmail.com>
2018-10-28 13:55:10 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,20 +1,36 @@
 import json
+import pandas as pd
+import numpy as np
+
+def load_data(file):
+    data = []
+    json_data = json.loads(file)
+    
+    for i in json_data['features']:
+        latitude = i['geometry']['coordinates'][0]
+        longitude = i['geometry']['coordinates'][1]
+        size = i['properties']['Attributes']['SeatsCount']
+        names = i['properties']['Attributes']['Name']
+        adress = i['properties']['Attributes']['Address']
+        data_string = names, adress, latitude, longitude, size
+        data.append(data_string)
+    
+    df = pd.DataFrame(data, columns=['Name', 'Address', 'Latitude', 'Longtitude', 'Size'])
+    
+    return df
 
 
-def load_data(filepath):
-    pass
+def get_biggest_bar(df):
+    return df[df['Size'] == max(df['Size'])]['Name'].to_string(index=False, header=False)
 
 
-def get_biggest_bar(data):
-    pass
+def get_smallest_bar(df):
+    return df[df['Size'] == min(df['Size'])]['Name'].to_string(index=False, header=False)
 
 
-def get_smallest_bar(data):
-    pass
-
-
-def get_closest_bar(data, longitude, latitude):
-    pass
+def closest_bar(df, My_Longtitude, My_Latitude):
+    df['Distance'] = np.sqrt(((df['My_Latitude'] - df['Longtitude'])**2) + ((df['My_Longtitude'] - df['Latitude'])**2))
+    return df.loc[df['Distance'] == min(df['Distance'])]['Name'].to_string(index=False, header=False)
 
 
 if __name__ == '__main__':
